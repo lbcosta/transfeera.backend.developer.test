6ff1eacf6b443b3e1f0be0ab4ec58fbfd511ef26
Leonardo Costa <leonardo.costa@guru.com.vc>
2023-03-27 14:42:51 -0300

    Adding color for tests output

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 cleanup() {
@@ -14,16 +15,24 @@ trap cleanup SIGINT
 
 cd "$(dirname "$0")" || exit
 
-go test ../src/api/v1/handlers
+output=$(go test -v ../src/api/v1/handlers 2>&1)
 if [ $? -ne 0 ]; then
+    echo -e "${output}" | awk '/FAIL/ {print "'${RED}'" $0 "'${NC}'"}; /PASS/ {print "'${GREEN}'" $0 "'${NC}'"}; /RUN / {print "'${YELLOW}'" $0 "'${NC}'"}; !/FAIL|PASS|RUN / {print}'
     echo -e "${RED}Error: Tests failed in 'handlers'. Check the test output above for details.${NC}"
     exit 1
+else
+        echo -e "${output}" | awk '/FAIL/ {print "'${RED}'" $0 "'${NC}'"}; /PASS/ {print "'${GREEN}'" $0 "'${NC}'"}; /RUN / {print "'${YELLOW}'" $0 "'${NC}'"}; !/FAIL|PASS|RUN / {print}'
+
 fi
 
-go test ../src/api/v1/services
+output=$(go test -v ../src/api/v1/services 2>&1)
 if [ $? -ne 0 ]; then
+    echo -e "${output}" | awk '/FAIL/ {print "'${RED}'" $0 "'${NC}'"}; /PASS/ {print "'${GREEN}'" $0 "'${NC}'"}; /RUN / {print "'${YELLOW}'" $0 "'${NC}'"}; !/FAIL|PASS|RUN / {print}'
     echo -e "${RED}Error: Tests failed in 'services'. Check the test output above for details.${NC}"
     exit 1
+else
+        echo -e "${output}" | awk '/FAIL/ {print "'${RED}'" $0 "'${NC}'"}; /PASS/ {print "'${GREEN}'" $0 "'${NC}'"}; /RUN / {print "'${YELLOW}'" $0 "'${NC}'"}; !/FAIL|PASS|RUN / {print}'
+
 fi
 
 docker-compose up --build -d
